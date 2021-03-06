1b5e1d2659d0f745bf0cb6479d6d801684062b85
infamoussoap <james.chok1000+github@gmail.com>
2021-03-06 13:21:46 +1100

    Model had a bug in checking if it was a valid model

--- PySoap2/models/Model.py
+++ PySoap2/models/Model.py
@@ -1,13 +1,18 @@
 class Model:
 
     @staticmethod
-    def _is_valid_model(input_layer, output_layer):
+    def _is_valid_model(start_layer, end_layer):
         """ Checks to see if there is a valid that connects the input layer to the output layer """
-        if len(input_layer.children) == 0:
-            return input_layer == output_layer
+        if len(start_layer.children) == 0:
+            return start_layer == end_layer
+        if end_layer in start_layer.children:
+            return True
 
-        return any([Model._is_valid_model(child, output_layer) for child in input_layer.children])
+        return any([Model._is_valid_model(child, end_layer) for child in start_layer.children])
 
     def __init__(self, input_layer, output_layer):
+        if not self._is_valid_model(input_layer, output_layer):
+            raise ValueError('There is no path from the input layer to the output layer.')
+
         self.input_layer = input_layer
         self.output_layer = output_layer
