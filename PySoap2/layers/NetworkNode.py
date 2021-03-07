aa0a553d4fb2fc60065d4ba4c6acb7debc516856
infamoussoap <james.chok1000+github@gmail.com>
2021-03-07 13:18:49 +1100

    Docustring added

--- PySoap2/layers/NetworkNode.py
+++ PySoap2/layers/NetworkNode.py
@@ -4,20 +4,61 @@ class NetworkNode(object):
         self.children = ()
 
     def add_parent(self, parent):
+        """ Add parent node
+
+            Parameters
+            ---------
+            parent : :obj:
+        """
         self.parents += (parent,)
 
     def add_parents(self, parents):
+        """ Add multiple parent nodes
+
+            Parameters
+            ---------
+            parents : tuple of :obj:
+        """
+
         self.parents += parents
 
     def add_child(self, child):
+        """ Add child node
+
+            Parameters
+            ---------
+            child : :obj:
+        """
         self.children += (child,)
 
     def add_children(self, children):
+        """ Add multiple child nodes
+
+            Parameters
+            ---------
+            children : tuple of :obj:
+        """
         self.children += children
 
     def __call__(self, parent_node):
+        """ Connects the current node instance with a single parent node
+
+            Parameters
+            ----------
+            parent_node : :obj:
+
+            Raises
+            ------
+            ValueError
+                If `parent_node` is a tuple, or
+                If it is not an instance of NetworkNode
+         """
+        if isinstance(parent_node, tuple):
+            raise ValueError('Call assumes a single object as the parent. '
+                             'Overload __call__ if you wish to initialise with tuple')
+
         if not isinstance(parent_node, NetworkNode):
-            raise ValueError('Call argument must be of type NetworkNode')
+            raise ValueError('Call argument must inherit from NetworkNode')
 
         self.add_parent(parent_node)
         parent_node.add_child(self)
