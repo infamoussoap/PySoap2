bbb84c32e1c2db4634c10ef18edc04c492e2185a
infamoussoap <james.chok1000+github@gmail.com>
2021-03-07 17:10:16 +1100

    model predict method is now created

--- PySoap2/models/Model.py
+++ PySoap2/models/Model.py
@@ -35,11 +35,46 @@ class Model:
         self.loss_function = loss_function
         self.metric_function = metrics
 
-        self.build_layer_(self.output_layer)
+        self._build_layer(self.output_layer)
 
     @staticmethod
-    def build_layer_(layer):
+    def _build_layer(layer):
         for parent in layer.parents:
             if not parent.built:
-                Model.build_layer_(parent)
+                Model._build_layer(parent)
         layer.build()
+
+    def predict(self, z, output_only=True):
+        cached_outputs = {self.input_layer.memory_location: self.input_layer.predict(z, output_only=output_only)}
+
+        model_prediction = self._propagated_output_of_layer(self.output_layer, cached_outputs, output_only=output_only)
+        if not output_only:
+            return cached_outputs
+        return model_prediction
+
+    @staticmethod
+    def _propagated_output_of_layer(layer, cached_outputs, output_only=True):
+        unique_identifier = layer.memory_location
+
+        if unique_identifier not in cached_outputs:
+            if len(layer.parents) == 0: 
+                raise ValueError(f'{layer} has no parent nodes')
+
+            layer_arg = Model._get_layer_argument(layer, cached_outputs, output_only=output_only)
+            cached_outputs[unique_identifier] = layer.predict(layer_arg, output_only=output_only)
+
+        return cached_outputs[unique_identifier]
+
+    @staticmethod
+    def _get_layer_argument(layer, cached_outputs, output_only=True):
+        outputs_of_parents = [Model._propagated_output_of_layer(parent, cached_outputs, output_only=output_only)
+                              for parent in layer.parents]
+
+        if not output_only:
+            layer_args = [post_activation for (pre_activation, post_activation) in outputs_of_parents]
+        else:
+            layer_args = outputs_of_parents
+
+        if len(outputs_of_parents) == 1:
+            return layer_args[0]
+        return layer_args
