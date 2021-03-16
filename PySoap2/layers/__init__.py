ab41c4dc1661ca9cc2c8dcda4044594bcc05c85a
infamoussoap <james.chok1000+github@gmail.com>
2021-03-16 17:48:52 +1100

    __init__ of layers now imports the Split children, and ConcatenateParent - done to make it easier when loading the model

--- PySoap2/layers/__init__.py
+++ PySoap2/layers/__init__.py
@@ -18,3 +18,8 @@ from .Flatten import Flatten
 # Split and Concatenate layers
 from .Split import Split
 from .Concatenate import Concatenate
+
+# Technical Layers for the Split and Concatenate Nodes
+from .Split import SplitLeftChild
+from .Split import SplitRightChild
+from .Concatenate import ConcatenateParent
