288e9cf2fe246ab0ee2a57924cdae76e8c4ca11c
infamoussoap <james.chok1000+github@gmail.com>
2021-04-29 14:00:18 +1000

    filled the code for some of the methods

--- PySoap2_gpu/layers/Dense.py
+++ PySoap2_gpu/layers/Dense.py
@@ -3,11 +3,12 @@ import numpy as np
 import pyopencl as cl
 import pyopencl.array as cl_array
 
-from PySoap2.layers import Layer
-from PySoap2.layers.NetworkNode import NetworkNode
-from PySoap2.layers.LayerBaseAttributes import LayerBaseAttributes
+from PySoap2_gpu.layers import Layer
+from PySoap2_gpu.layers.NetworkNode import NetworkNode
+from PySoap2_gpu.layers.LayerBaseAttributes import LayerBaseAttributes
 
 from dense_c_code import dense_source_code
+from .ValueChecks import assert_instance_of_cl_array
 
 
 class Dense(NetworkNode, LayerBaseAttributes, Layer):
@@ -35,6 +36,9 @@ class Dense(NetworkNode, LayerBaseAttributes, Layer):
         self.W_device = None
         self.b_device = None
 
+        self.input_length_device = None
+        self.output_length_device = None
+
     def build(self, gpu_context, gpu_queue):
         """ Initialises the weight and bias units """
 
@@ -59,24 +63,70 @@ class Dense(NetworkNode, LayerBaseAttributes, Layer):
         self.W_device = cl_array.to_device(self.gpu_queue, W)
         self.b_device = cl_array.to_device(self.gpu_queue, b)
 
-
-        self.dense_program = cl.Program(self.gpu_context, dense_source_code).build()
+        self.gpu_program = cl.Program(self.gpu_context, dense_source_code).build()
 
         self.built = True
 
     def predict(self, z_device, output_only=True, **kwargs):
-        if not isinstance(z_device, cl_array.Array):
-            raise ValueError(f'Input must be an instance of pyopencl.array.Array not {type(z)}')
+        assert_instance_of_cl_array(z_device)
 
         N = len(z_device)
 
         out_device = cl_array.empty(self.gpu_queue, (N, *self.output_shape), dtype=np.float32)
 
-        args = [z_device, self.W_device, self.b_device, self.input_length_device, self.output_length_device]
-        args_data = [arg.data for arg in args] + [out_device.data]
+        args = [z_device, self.W_device, self.b_device, self.input_length_device, self.output_length_device
+                out_device]
+        args_data = [arg.data for arg in args]
 
-        self.dense_program.predict(self.gpu_queue, (N, *self.output_shape), None, *args_data)
+        self.gpu_program.predict(self.gpu_queue, (N, *self.output_shape), None, *args_data)
 
         if output_only:
             return self.activation_function_(out_device)
         return out_device, self.activation_function_(out_device)
+
+    def get_delta_backprop_(self, g_prime_device, delta_device, *args):
+        assert_instance_of_cl_array(g_prime_device)
+        assert_instance_of_cl_array(delta_device)
+
+        out_device = cl_array.empty(self.gpu_queue, g_prime_device.shape, dtype=np.float32)
+
+        args = [g_prime_device, delta_device, self.W_device, self.input_length_device,
+                self.output_length_device, out_device]
+        args_data = [arg.data for arg in args]
+
+        self.gpu_program.delta_back_prop(self.gpu_queue, g_prime_device.shape, None, *args_data)
+
+        return out_device
+
+    def get_parameter_gradients_(self, delta_device, z_device):
+        assert_instance_of_cl_array(delta_device)
+        assert_instance_of_cl_array(z_device)
+
+        N = np.array(len(z_device)).astype(np.int32)
+        N_device = cl_array.to_device(self.gpu_queue, N)
+
+        W_grad_device = cl_array.empty(self.gpu_queue, self.W_device.shape, dtype=np.float32)
+        args = [delta_device, z_device, self.input_length_device, self.output_length_device,
+                N_device, W_grad_device]
+        args_data = [arg.data for arg in args]
+        self.gpu_program.weight_gradient(self.gpu_queue, self.W_device.shape, None, *args_data)
+
+        b_grad_device = cl_array.empty(self.gpu_queue, self.b_device.shape, dtype=np.float32)
+        args = [delta_device, self.output_length_device, N_device, b_grad_device]
+        args_data = [arg.data for arg in args]
+        self.gpu_program.bias_gradient(self.gpu_queue, self.output_shape, None, *args_data)
+
+        parameter_gradients = {'weight': W_grad_device, 'bias': b_grad_device}
+        return parameter_gradients
+
+    def update_parameters_(self, parameter_updates):
+        pass
+
+    def get_weights(self):
+        pass
+
+    def summary_(self):
+        return f'Dense {(self.hidden_nodes,)}', f'Output Shape {(None, *self.output_shape)}'
+
+    def __str__(self):
+        return f'Dense: Output Shape {(None, *self.output_shape)}'
