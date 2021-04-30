32720f8f8dea96d07f9fbe6aee25c7601bef51da
infamoussoap <james.chok1000+github@gmail.com>
2021-04-30 23:38:09 +1000

    updated init file and pycache file

--- PySoap2_gpu/__init__.py
+++ PySoap2_gpu/__init__.py
@@ -1,3 +1,7 @@
 from .functions import get_activation_function
+from .functions import get_metric_function
+from .functions import get_error_function
 
 import PySoap2_gpu.layers as layers
+
+import PySoap2_gpu.optimizers as optimizers
---
+++ PySoap2_gpu/optimizers/__init__.py
@@ -0,0 +1,6 @@
+# Optimizer Abstract class
+from .Optimizer import Optimizer
+
+from .Adam import Adam
+
+from .get_optimizer import get_optimizer
