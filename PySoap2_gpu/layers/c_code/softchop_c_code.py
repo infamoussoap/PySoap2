24782e835bc170a882f540aa4fa04197adb2e02c
infamoussoap <james.chok1000+github@gmail.com>
2021-04-30 20:55:40 +1000

    softchop c code had a bug

--- PySoap2_gpu/layers/c_code/softchop_c_code.py
+++ PySoap2_gpu/layers/c_code/softchop_c_code.py
@@ -7,15 +7,15 @@ __kernel void delta_back_prop(__global const float *g_prime, __global const floa
 }
 
 
-__kernel void parameter_gradient(__global cost float *delta, __global const float *parameter, 
+__kernel void parameter_gradient(__global const float *delta, __global const float *parameter, 
                                  __global int *inputLength, __global int *N1, __global float *out)
 {
     int input_length = *inputLength;
-    int N = *N1
+    int N = *N1;
     int i = get_global_id(0);
 
     float total = 0.0;
-    for (int n = 0; n < N1; n++)
+    for (int n = 0; n < N; n++)
     {
         total += delta[n*input_length + i] * parameter[n*input_length + i];
     }
