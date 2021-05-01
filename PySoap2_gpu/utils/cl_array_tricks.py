e5a3ce24024b96c63d8766f5265ab8807c3ea717
infamoussoap <james.chok1000+github@gmail.com>
2021-05-01 12:45:43 +1000

    refactored to fit PEP guidelines

--- PySoap2_gpu/utils/cl_array_tricks.py
+++ PySoap2_gpu/utils/cl_array_tricks.py
@@ -7,14 +7,14 @@ device_context = cl.Context([device])
 device_queue = cl.CommandQueue(device_context)
 
 clip_cl_array_by_min_value_in_place = cl.elementwise.ElementwiseKernel(device_context,
-                                                                    "float *x, float threshold",
-                                                                    "x[i] = x[i] > threshold ? x[i] : threshold",
-                                                                    "clip_in_place_elementwise")
+                                                                       "float *x, float threshold",
+                                                                       "x[i] = x[i] > threshold ? x[i] : threshold",
+                                                                       "clip_in_place_elementwise")
 
 clip_cl_array_by_max_value_in_place = cl.elementwise.ElementwiseKernel(device_context,
-                                                                    "float *x, float threshold",
-                                                                    "x[i] = x[i] < threshold ? x[i] : threshold",
-                                                                    "clip_in_place_elementwise")
+                                                                       "float *x, float threshold",
+                                                                       "x[i] = x[i] < threshold ? x[i] : threshold",
+                                                                       "clip_in_place_elementwise")
 
 
 def clip_cl_array_in_place(array, min_val, max_val):
