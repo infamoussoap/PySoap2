5fe5f5bbbe2ce71783e0f80bcb620418e3278017
infamoussoap <james.chok1000+github@gmail.com>
2021-05-03 19:11:39 +1000

    array tricks and math functions used to compile everytime they were called. Now they are bound to a class, so compliation occurs at first initialisation of the class

--- PySoap2_gpu/utils/cl_array_tricks.py
+++ PySoap2_gpu/utils/cl_array_tricks.py
@@ -1,23 +1,71 @@
+import numpy as np
 import pyopencl as cl
 
+from pyopencl.elementwise import ElementwiseKernel
+import pyopencl.array as cl_array
 
-def clip_cl_array_by_min_value_in_place(device_context):
-    return cl.elementwise.ElementwiseKernel(device_context,
-                                            "float *x, float threshold",
-                                            "x[i] = x[i] > threshold ? x[i] : threshold",
-                                            "clip_in_place_elementwise")
+from .cl_math_functions_c_code import cl_array_max_source_code
 
 
-def clip_cl_array_by_max_value_in_place(device_context):
-    return cl.elementwise.ElementwiseKernel(device_context,
-                                            "float *x, float threshold",
-                                            "x[i] = x[i] < threshold ? x[i] : threshold",
-                                            "clip_in_place_elementwise")
+class ClArrayTricks:
+    initialized = False
 
+    clip_cl_array_by_min_value_in_place = None
+    clip_cl_array_by_max_value_in_place = None
+    cl_array_max_program = None
 
-def clip_cl_array_in_place(device_context, array, min_val, max_val):
-    if min_val is not None:
-        clip_cl_array_by_min_value_in_place(device_context)(array, min_val)
+    device_context = None
+    device_queue = None
 
-    if max_val is not None:
-        clip_cl_array_by_max_value_in_place(device_context)(array, max_val)
+    def __init__(self, device_context, device_queue):
+        ClArrayTricks.device_context = device_context
+        ClArrayTricks.device_queue = device_queue
+
+        ClArrayTricks.clip_cl_array_by_min_value_in_place = ElementwiseKernel(device_context,
+                                                                              "float *x, float threshold",
+                                                                              "x[i] = x[i] > threshold ? "
+                                                                              "x[i] : threshold",
+                                                                              "clip_in_place_elementwise")
+
+        ClArrayTricks.clip_cl_array_by_max_value_in_place = ElementwiseKernel(device_context,
+                                                                              "float *x, float threshold",
+                                                                              "x[i] = x[i] < threshold ? "
+                                                                              "x[i] : threshold",
+                                                                              "clip_in_place_elementwise")
+
+        ClArrayTricks.cl_array_max_program = cl.Program(device_context, cl_array_max_source_code).build()
+
+        ClArrayTricks.initialized = True
+
+    @staticmethod
+    def clip_cl_array_in_place(array, min_val, max_val):
+        if min_val is not None:
+            ClArrayTricks.clip_cl_array_by_max_value_in_place(array, min_val)
+
+        if max_val is not None:
+            ClArrayTricks.clip_cl_array_by_max_value_in_place(array, max_val)
+
+    @staticmethod
+    def max_across_last_axis(x_gpu):
+        last_axis_length = cl_array.to_device(ClArrayTricks.device_queue, np.array(x_gpu.shape[-1], dtype=np.int32))
+        out_gpu = cl_array.empty(ClArrayTricks.device_queue, x_gpu.shape[:-1], dtype=np.float32)
+
+        event = ClArrayTricks.cl_array_max_program.max_across_last_axis(ClArrayTricks.device_queue,
+                                                                        (np.prod(out_gpu.shape),), None,
+                                                                        x_gpu.data, last_axis_length.data, out_gpu.data)
+        event.wait()
+
+        return out_gpu
+
+    @staticmethod
+    def arg_max_across_last_axis(x_gpu):
+        last_axis_length = cl_array.to_device(ClArrayTricks.device_queue, np.array(x_gpu.shape[-1], dtype=np.int32))
+        out_gpu = cl_array.empty(ClArrayTricks.device_queue, x_gpu.shape[:-1], dtype=np.int32)
+
+        event = ClArrayTricks.cl_array_max_program.arg_max_across_last_axis(ClArrayTricks.device_queue,
+                                                                            (np.prod(out_gpu.shape),), None,
+                                                                            x_gpu.data, last_axis_length.data,
+                                                                            out_gpu.data)
+        event.wait()
+
+        return out_gpu
--- PySoap2_gpu/utils/cl_math_functions.py
+++ PySoap2_gpu/utils/cl_math_functions.py
@@ -2,73 +2,80 @@ import numpy as np
 
 import pyopencl as cl
 import pyopencl.array as cl_array
+from pyopencl.elementwise import ElementwiseKernel
 
 from .cl_math_functions_c_code import softmax_source_code
-from .cl_math_functions_c_code import cl_array_max_source_code
+from .cl_array_tricks import ClArrayTricks
 
 
-def elementwise_relu(device_context):
-    return cl.elementwise.ElementwiseKernel(device_context, "float *x, float *out",
-                                            "out[i] = x[i] > 0 ? x[i] : 0.0", "relu")
+class ClMathFunctions:
+    initialized = False
 
+    device_context = None
+    device_queue = None
 
-def elementwise_relu_grad(device_context):
-    return cl.elementwise.ElementwiseKernel(device_context, "float *x, float *out",
-                                            "out[i] = x[i] > 0 ? 1.0 : 0.0", "relu")
+    relu_program = None
+    relu_grad_program = None
+    sigmoid_program = None
+    softmax_gpu_program = None
 
+    def __init__(self, device_context, device_queue):
+        ClMathFunctions.device_context = device_context
+        ClMathFunctions.device_queue = device_queue
 
-def elementwise_sigmoid(device_context):
-    return cl.elementwise.ElementwiseKernel(device_context,
-                                            "float *x, float *out",
-                                            "out[i] = SIGMOID(x[i])",
-                                            "sigmoid",
-                                            preamble='#define SIGMOID(x) x > 0 ? '
-                                                     '1.0/(1.0 + exp(-x)) : exp(x) / (exp(x) + 1.0)'
-                                            )
+        ClMathFunctions.relu_program = ElementwiseKernel(device_context, "float *x, float *out",
+                                                         "out[i] = x[i] > 0 ? x[i] : 0.0", "relu")
 
+        ClMathFunctions.relu_grad_program = ElementwiseKernel(device_context, "float *x, float *out",
+                                                              "out[i] = x[i] > 0 ? 1.0 : 0.0", "relu")
 
-def softmax_gpu(device_context, device_queue, x_gpu, grad=False):
-    softmax_gpu_program = cl.Program(device_context, softmax_source_code).build()
+        ClMathFunctions.sigmoid_program = ElementwiseKernel(device_context,
+                                                            "float *x, float *out",
+                                                            "out[i] = SIGMOID(x[i])",
+                                                            "sigmoid",
+                                                            preamble='#define SIGMOID(x) x > 0 ? '
+                                                                     '1.0/(1.0 + exp(-x)) : exp(x) / (exp(x) + 1.0)'
+                                                            )
 
-    if grad:
-        softmax_val = softmax_gpu(device_context, device_queue, x_gpu, grad=False)
-        return softmax_val * (1 - softmax_val)
+        ClMathFunctions.softmax_gpu_program = cl.Program(device_context, softmax_source_code).build()
 
-    input_length_gpu = cl_array.to_device(device_queue, np.array(x_gpu.shape[-1], dtype=np.int32))
-    max_val_gpu = max_across_last_axis(device_context, device_queue, x_gpu)
+        ClMathFunctions.initialized = True
 
-    softmax_val = cl_array.empty_like(x_gpu)
+        if not ClArrayTricks.initialized:
+            ClArrayTricks(self.device_context, self.device_queue)
 
-    global_shape_col = x_gpu.shape[-1]
-    global_shape_row = np.prod(x_gpu.shape[:-1]).astype(np.int32)
-    global_shape = (global_shape_row, global_shape_col)
+    @staticmethod
+    def relu(x_gpu):
+        out_gpu = cl_array.empty_like(x_gpu)
+        ClMathFunctions.relu_program(x_gpu, out_gpu)
+        return out_gpu
 
-    event = softmax_gpu_program.softmax(device_queue, global_shape, None,
-                                        x_gpu.data, max_val_gpu.data, input_length_gpu.data, softmax_val.data)
-    event.wait()
+    @staticmethod
+    def relu_grad(x_gpu):
+        out_gpu = cl_array.empty_like(x_gpu)
+        ClMathFunctions.relu_grad_program(x_gpu, out_gpu)
+        return out_gpu
 
-    return softmax_val
+    @staticmethod
+    def sigmoid(x_gpu):
+        out_gpu = cl_array.empty_like(x_gpu)
+        ClMathFunctions.sigmoid_program(x_gpu, out_gpu)
+        return out_gpu
 
+    @staticmethod
+    def softmax(x_gpu):
+        input_length_gpu = cl_array.to_device(ClMathFunctions.device_queue, np.array(x_gpu.shape[-1], dtype=np.int32))
+        max_val_gpu = ClArrayTricks.max_across_last_axis(x_gpu)
 
-def max_across_last_axis(device_context, device_queue, x_gpu):
-    cl_array_max_program = cl.Program(device_context, cl_array_max_source_code).build()
+        softmax_val = cl_array.empty_like(x_gpu)
 
-    last_axis_length = cl_array.to_device(device_queue, np.array(x_gpu.shape[-1], dtype=np.int32))
-    out_gpu = cl_array.empty(device_queue, x_gpu.shape[:-1], dtype=np.float32)
+        global_shape_col = x_gpu.shape[-1]
+        global_shape_row = np.prod(x_gpu.shape[:-1]).astype(np.int32)
+        global_shape = (global_shape_row, global_shape_col)
 
-    event = cl_array_max_program.max_across_last_axis(device_queue, (np.prod(out_gpu.shape),), None,
-                                                      x_gpu.data, last_axis_length.data, out_gpu.data)
-    event.wait()
-    return out_gpu
+        event = ClMathFunctions.softmax_gpu_program.softmax(ClMathFunctions.device_queue, global_shape, None,
+                                                            x_gpu.data, max_val_gpu.data, input_length_gpu.data,
+                                                            softmax_val.data)
+        event.wait()
 
-
-def arg_max_across_last_axis(device_context, device_queue, x_gpu):
-    cl_array_max_program = cl.Program(device_context, cl_array_max_source_code).build()
-
-    last_axis_length = cl_array.to_device(device_queue, np.array(x_gpu.shape[-1], dtype=np.int32))
-    out_gpu = cl_array.empty(device_queue, x_gpu.shape[:-1], dtype=np.int32)
-
-    event = cl_array_max_program.arg_max_across_last_axis(device_queue, (np.prod(out_gpu.shape),), None,
-                                                          x_gpu.data, last_axis_length.data, out_gpu.data)
-    event.wait()
-    return out_gpu
+        return softmax_val
