9da3c8196cc3f3ffb464d9bb45c3370a5eba34b6
infamoussoap <james.chok1000+github@gmail.com>
2021-05-03 19:12:31 +1000

    functions updated to reflect changes in cl_math_functions and cl_array_tricks

--- PySoap2_gpu/functions.py
+++ PySoap2_gpu/functions.py
@@ -6,82 +6,125 @@ from pyopencl import clmath
 
 from functools import partial
 
-from PySoap2_gpu.utils import clip_cl_array_in_place
-
-from PySoap2_gpu.utils import cl_math_functions
-
-
-def get_activation_function(device_context, device_queue, name):
-    if device_context is None:
-        raise ValueError('Context for gpu cannot be None.')
-
-    if name == 'relu':
-        def relu(x_device, grad=False):
-            """ x is assumed to be an instance of cl.array.Array"""
-            out_device = cl_array.empty_like(x_device)
-            if grad:
-                cl_math_functions.elementwise_relu_grad(device_context)(x_device, out_device)
-            else:
-                cl_math_functions.elementwise_relu(device_context)(x_device, out_device)
-            return out_device
-
-        return relu
-
-    elif name == 'linear':
-        def linear(x_device, grad=False):
-            if grad:
-                return cl_array.zeros_like(x_device) + 1.0
-            return x_device
-        return linear
-    elif name == 'softmax':
-        return partial(cl_math_functions.softmax_gpu, device_context, device_queue)
-
-    elif name == 'sigmoid':
-        def sigmoid(x_device, grad=False):
-            out_device = cl_array.empty_like(x_device)
-            cl_math_functions.elementwise_sigmoid(device_context)(x_device, out_device)
-            if grad:
-                return out_device * (1 - out_device)
-            return out_device
-
-        return sigmoid
-
-    else:
-        raise Exception(f'{name} is not a defined function.')
+from PySoap2_gpu.utils import ClMathFunctions
+from PySoap2_gpu.utils import ClArrayTricks
 
 
-def get_error_function(device_context, device_queue, name):
-    if name == 'mse':
-        def mse(predictions, targets, grad=False):
-            if grad:
-                return 2 * (predictions - targets)
-            N = predictions.shape[0]
-            return np.sum(((predictions - targets) ** 2) / 2) / N
+class ActivationFunction:
+    initialized = False
 
-        return mse
-    elif name == 'cross_entropy':
-        def cross_entropy(predictions, targets, epsilon=1e-12, grad=False):
-            clip_cl_array_in_place(device_context, predictions, epsilon, 1.0 - epsilon)
+    device_context = None
+    device_queue = None
 
-            if grad:
-                return -targets / predictions + (1.0 - targets) / (1.0 - predictions)
+    def __init__(self, device_context, device_queue):
+        ActivationFunction.device_context = device_context
+        ActivationFunction.device_queue = device_queue
 
-            N = predictions.shape[0]
-            ce = -np.sum(targets * clmath.log(predictions + 1e-9)) / N
-            return ce
+        if not ClMathFunctions.initialized:
+            ClMathFunctions(device_context, device_queue)
 
-        return cross_entropy
+        ActivationFunction.initialized = True
+
+    @staticmethod
+    def get_activation_function(name):
+        if name == 'relu':
+            def relu(x_device, grad=False):
+                if grad:
+                    return ClMathFunctions.relu_grad(x_device)
+                return ClMathFunctions.relu(x_device)
+
+            return relu
+        elif name == 'linear':
+            def linear(x_device, grad=False):
+                if grad:
+                    return cl_array.zeros_like(x_device) + 1.0
+                return x_device
+
+            return linear
+        elif name == 'softmax':
+            def softmax(x_device, grad=False):
+                softmax_val = ClMathFunctions.softmax(x_device)
+                if grad:
+                    return softmax_val * (1 - softmax_val)
+                return softmax_val
+
+            return softmax
+        elif name == 'sigmoid':
+            def sigmoid(x_device, grad=False):
+                sigmoid_val = ClMathFunctions.sigmoid(x_device)
+                if grad:
+                    return sigmoid_val * (1 - sigmoid_val)
+                return sigmoid_val
+
+            return sigmoid
+
+        else:
+            raise Exception(f'{name} is not a defined function.')
 
 
-def get_metric_function(device_context, device_queue, name):
-    if name == 'accuracy':
-        def accuracy(predictions, target):
-            predicted_labels = cl_math_functions.arg_max_across_last_axis(device_context, device_queue, predictions)
-            target_labels = cl_math_functions.arg_max_across_last_axis(device_context, device_queue, target)
+class ErrorFunction:
+    initialized = False
 
-            N = len(predictions)
-            return cl_array.sum((predicted_labels == target_labels)).astype(np.float32)/N
+    device_context = None
+    device_queue = None
 
-        return accuracy
-    else:
-        raise Exception(f'{name} is not a defined metric.')
+    def __init__(self, device_context, device_queue):
+        ErrorFunction.device_queue = device_queue
+        ErrorFunction.device_context = device_context
+
+        if not ClArrayTricks.initialized:
+            ClArrayTricks(device_context, device_queue)
+
+        ErrorFunction.initialized = True
+
+    @staticmethod
+    def get_error_function(name):
+        if name == 'mse':
+            def mse(predictions, targets, grad=False):
+                if grad:
+                    return 2 * (predictions - targets)
+                N = predictions.shape[0]
+                return np.sum(((predictions - targets) ** 2) / 2) / N
+
+            return mse
+        elif name == 'cross_entropy':
+            def cross_entropy(predictions, targets, epsilon=1e-12, grad=False):
+                ClArrayTricks.clip_cl_array_in_place(predictions, epsilon, 1.0 - epsilon)
+                if grad:
+                    return -targets / predictions + (1.0 - targets) / (1.0 - predictions)
+
+                N = predictions.shape[0]
+                ce = -np.sum(targets * clmath.log(predictions + 1e-9)) / N
+                return ce
+
+            return cross_entropy
+
+
+class MetricFunction:
+    initialized = False
+
+    device_context = None
+    device_queue = None
+
+    def __init__(self, device_context, device_queue):
+        MetricFunction.device_queue = device_queue
+        MetricFunction.device_context = device_context
+
+        if not ClArrayTricks.initialized:
+            ClArrayTricks(device_context, device_queue)
+
+        MetricFunction.initialized = True
+
+    @staticmethod
+    def get_metric_function(name):
+        if name == 'accuracy':
+            def accuracy(predictions, target):
+                predicted_labels = ClArrayTricks.arg_max_across_last_axis(predictions)
+                target_labels = ClArrayTricks.arg_max_across_last_axis(target)
+
+                N = len(predictions)
+                return cl_array.sum((predicted_labels == target_labels)).astype(np.float32)/N
+
+            return accuracy
+        else:
+            raise Exception(f'{name} is not a defined metric.')
