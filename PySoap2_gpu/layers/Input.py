352f4872119e008cd1c1a05d737d1c41a252be0a
infamoussoap <james.chok1000+github@gmail.com>
2021-05-12 11:08:57 +1000

    Input predict was only returning 1 value when output_only=False, should be returning 2. get_parameter_gradients now returns empty dictionary

--- PySoap2_gpu/layers/Input.py
+++ PySoap2_gpu/layers/Input.py
@@ -6,25 +6,6 @@ from .ValueChecks import assert_instance_of_cl_array
 
 
 class Input(NetworkNode, LayerBaseAttributes, Layer):
-    """ This class will determine the input dimensions of the dataset
-
-        Notes
-        -----
-        This class must always be the first `sequential_layer` that is added to an instance
-        of the `sequential` class. This sequential layer will be given the shape of the input
-        and will determine the shape of later `sequential_layer` in the network
-        Also, note that the input is essentially a pass through layer, that is not trainable
-
-        Attributes
-        ----------
-        input_shape : tuple of int
-            The input shape of a datapoint
-        output_shape : tuple of int
-            The output shape of this layer, which is equal to the `input_shape`
-        built : bool
-            Has this layer been initialised
-    """
-
     def __init__(self, input_shape):
         """ Creates a `Input` class with a given input shape
 
@@ -55,53 +36,19 @@ class Input(NetworkNode, LayerBaseAttributes, Layer):
         self.device_queue = device_queue
 
     def predict(self, z, *args, output_only=True, **kwargs):
-        """ Returns the output of this layer
-
-            Parameters
-            ----------
-            z : np.array
-                z is assumed to be a list of all the inputs to be forward propagated. In particular
-                it is assumed that the first index of z is the index that inputs is accessed by
-            output_only : :obj:`bool`, optional
-                If set to true, then this function will return only the prediction of the neural
-                network. If set to false then this will return the outputs of the individual
-                layers. Unless back propagation is being performed, this should be set to true.
-
-            Returns
-            -------
-            np.array
-                The final output of the layer
-
-            OR (if `output_only = False`)
-
-            np.array, np.array
-                The first np.array will store the output before it is passed through the activation
-                function.
-                The second np.array will store the output after it has passed through the
-                activation function.
-        """
         assert_instance_of_cl_array(z)
 
         if output_only:
             return z
-        else:
-            return z, z
+        return z, z
 
     def get_delta_backprop_(self, g_prime, new_delta, *args):
-        """ Since this layer has no parameters, there is no implementation needed
-        """
-
         return None
 
     def get_parameter_gradients_(self, delta, prev_z):
-        """ Since this layer has no parameters, there is no implementation needed """
-
-        parameter_gradients = {}
-        return parameter_gradients
+        return {}
 
     def update_parameters_(self, parameter_updates):
-        """ Since this layer has no parameters, there is no implementation needed """
-
         pass
 
     def get_weights(self):
