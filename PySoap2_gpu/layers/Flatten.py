b9283d5278fbd0f6ce862022b0dfdf96f847316f
infamoussoap <james.chok1000+github@gmail.com>
2021-05-12 11:07:51 +1000

    Flatten updated to now check inputs are instance of cl_array

--- PySoap2_gpu/layers/Flatten.py
+++ PySoap2_gpu/layers/Flatten.py
@@ -4,26 +4,10 @@ from PySoap2_gpu.layers import Layer
 from PySoap2_gpu.layers.NetworkNode import NetworkNode
 from PySoap2_gpu.layers.LayerBaseAttributes import LayerBaseAttributes
 
+from .ValueChecks import assert_instance_of_cl_array
+
 
 class Flatten(NetworkNode, LayerBaseAttributes, Layer):
-    """ Given a n-dimensional input, this layer will return the flatten representation
-        of the input
-
-        Attributes
-        ----------
-        input_shape : tuple
-            The input shape
-        output_shape : 1 tuple
-            The output shape
-        built : bool
-            Has the layer been initialised
-
-        Notes
-        -----
-        When a n-dimensional input is fed into a `Dense` layer, it needs to be flattened
-        into a vector first. This `Flatten` class performs such flattening
-    """
-
     def __init__(self):
         NetworkNode.__init__(self)
         LayerBaseAttributes.__init__(self)
@@ -41,85 +25,22 @@ class Flatten(NetworkNode, LayerBaseAttributes, Layer):
         self.built = True
 
     def predict(self, z, output_only=True, pre_activation_of_input=None):
-        """ Returns the prediction of this layer
-
-            Parameters
-            ----------
-            z : (N, *input_shape) np.array
-                The input to be flattened
-            output_only : bool, optional
-                If set to true, then this function will return only the prediction of the neural
-                network. If set to false then this will return the outputs of the individual
-                layers. Unless back propagation is being performed, this should be set to true.
-            pre_activation_of_input : (N, *input_shape) np.array
-                The input, z, before it passed through the activation function
-
-            Returns
-            -------
-            (N, *output_shape) np.array
-                The flattened representation of the input
-
-            OR (if `output_only = False`)
-
-            (N, *input_shape) np.array, (N, *output_shape) np.array
-                The first np.array will store the output before it has been reshaped
-                The second np.array will store the output after it has been reshaped
-
-            Notes
-            -----
-            Since this layer has no activation function,
-        """
+        assert_instance_of_cl_array(z)
 
         if output_only:
             return z.reshape(-1, *self.output_shape)
         return pre_activation_of_input, z.reshape(-1, *self.output_shape)
 
     def get_delta_backprop_(self, g_prime, new_delta, *args, **kwargs):
-        """ Returns the delta for the previous layer, delta^{k-1}_{m,j}.
-
-            Parameters
-            ----------
-            g_prime : (N, *input_shape) np.array
-                Should be the derivative of the ouput of the previous layer, g'_{k-1}(a^{k-1}_{m,j})
-            new_delta : (N, *output_shape) np.array
-                The delta for this layer, delta^k_{m, j}
-
-            Returns
-            -------
-            (N, *input_shape) np.array
-
-            Notes
-            -----
-            Since this is a pass through layer (i.e. linear activation), g_prime = 1, and so can be ignored.
-            The key to this layer is that the delta of the k+1 layer needs to be reshaped
-            for the k-1 layer
-        """
+        assert_instance_of_cl_array(g_prime)
+        assert_instance_of_cl_array(new_delta)
 
         return new_delta.reshape(-1, *self.input_shape)
 
     def get_parameter_gradients_(self, *args, **kwargs):
-        """ Returns the associated partial S/partial W^k, that is
-            the gradient with respect to the weight matrix in the kth layer
-
-            Returns
-            -------
-            dict
-
-            Notes
-            -----
-            Since nothing in this layer is trainiable, the gradients is simply None
-        """
-
-        parameter_gradients = {}
-        return parameter_gradients
+        return {}
 
     def update_parameters_(self, parameter_gradients):
-        """ Perform an update to the weights by descending down the gradient
-
-            Notes
-            -----
-            Since nothing in this layer is trainiable, we can simply pass
-        """
         pass
 
     def get_weights(self):
