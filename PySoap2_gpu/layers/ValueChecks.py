ae9cf27df348b16707b05d546cac6b4ec80941f6
infamoussoap <james.chok1000+github@gmail.com>
2021-05-20 17:22:31 +1000

    check_built-gpu implemented

--- PySoap2_gpu/layers/ValueChecks.py
+++ PySoap2_gpu/layers/ValueChecks.py
@@ -4,3 +4,11 @@ import pyopencl.array as cl_array
 def assert_instance_of_cl_array(array):
     if not isinstance(array, cl_array.Array):
         raise ValueError(f'Arrays must be an instance of pyopencl.array.Array not {type(array)}')
+
+
+def check_built(function):
+    def wrapper(inst, *args, **kwargs):
+        if not inst.built:
+            raise ValueError(f'{type(inst).__name__} Layer must be built before it is used.')
+        return function(inst, *args, **kwargs)
+    return wrapper
