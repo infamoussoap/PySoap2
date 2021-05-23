a129a7d07ab925fc1edcff95739c5a0609947548
infamoussoap <james.chok1000+github@gmail.com>
2021-05-23 14:29:58 +1000

    Add-cpu layer added

---
+++ PySoap2/layers/Add.py
@@ -0,0 +1,73 @@
+from functools import reduce
+
+from PySoap2.layers import Layer
+from PySoap2.layers.NetworkNode import NetworkNode
+from PySoap2.layers.LayerBaseAttributes import LayerBaseAttributes
+
+from .LayerBuiltChecks import check_built
+
+
+class Add(NetworkNode, LayerBaseAttributes, Layer):
+    """ Add the input values
+
+        Notes
+        -----
+        The activation function is simply the linear function
+    """
+    def __init__(self):
+        NetworkNode.__init__(self)
+        LayerBaseAttributes.__init__(self)
+
+    def build(self):
+        """ Initialises the weight and bias units """
+        
+
+
+        self.input_shape = tuple(parent.output_shape for parent in self.parents)
+        self.output_shape = self.input_shape[0]
+
+        self.built = True
+
+    @check_built
+    def predict(self, z, output_only=True, pre_activation_of_input=None):
+        out = reduce(lambda x, y: x + y, z)
+
+        if output_only:
+            return out
+        return out, out
+
+    @check_built
+    def get_delta_backprop_(self, g_prime, new_delta, prev_z):
+        delta = reduce(lambda x, y: x + y, new_delta)
+        return delta
+
+    @check_built
+    def get_parameter_gradients_(self, delta, prev_z):
+        return {}
+
+    @check_built
+    def update_parameters_(self, parameter_updates):
+        pass
+
+    @check_built
+    def get_weights(self):
+        return None
+
+    @check_built
+    def summary_(self):
+        return 'Add', f'Output Shape {self.input_shape}'
+
+    def __call__(self, input_layers):
+        """ __call__ of NetworkNode is overloaded as it is now assumed the parameter
+            is a list of NetworkNode
+
+            Parameters
+            ----------
+            input_layers : list of NetworkNode
+        """
+        for parent_node in input_layers:
+            parent_node.add_child(self)
+
+        self.add_parents(tuple(input_layers))
+
+        return self
