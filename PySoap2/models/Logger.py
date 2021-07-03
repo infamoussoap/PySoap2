3f25971f7f6e8ffe99008e49f3c056a27fd3208b
infamoussoap <james.chok1000+github@gmail.com>
2021-07-03 13:51:45 +1000

    Logger now has documentation

--- PySoap2/models/Logger.py
+++ PySoap2/models/Logger.py
@@ -2,8 +2,32 @@ import pandas as pd
 
 
 class ModelLogger:
+    """ Logging module for model training
+
+        Notes
+        -----
+        auto_save is used by the `model` attribute
+            If true then the logger will save after training
+            Otherwise, you will need to call the `save` method to save the log
+
+        Attributes
+        ----------
+        model : :obj:Model
+            Either the cpu Model or gpu Model
+        x_train : np.array or cl_array.Array
+        y_train : np.array or cl_array.Array
+        x_test : np.array or cl_array.Array or None
+        y_test : np.array or cl_array.Array or None
+        auto_save : bool
+             If set to true, then ModelLogger will automatically save once training is finished
+             by the `model`
+        train_history : list[ModelEvalLog]
+            Saves the training history
+        test_history : list[ModelEvalLog]
+            Saves the testing history
+    """
     def __init__(self, model, x_train, y_train, x_test=None, y_test=None, auto_save=True):
-        """ auto_save is used by Model - If true then the logger will save after training """
+        """ Initialise the logger with the model and train/test sets """
         self.model = model
 
         self.x_train = x_train
@@ -18,10 +42,18 @@ class ModelLogger:
         self.test_history = []
 
     def log_model(self, epoch, batch_number=None):
+        """ Log (or save) the current train and test score of the model
+
+            Parameters
+            ----------
+            epoch : int
+            batch_number : int, optional
+        """
         self.log_train_score(epoch, batch_number=batch_number)
         self.log_test_score(epoch, batch_number=batch_number)
 
     def log_train_score(self, epoch, batch_number=None):
+        """ Log the current training score of the model """
         prediction = self.model.predict(self.x_train)
         loss = self.model._loss_function(prediction, self.y_train)
         metric = None if self.model._metric is None else self.model._metric(prediction, self.y_train)
@@ -29,6 +61,7 @@ class ModelLogger:
         self.train_history.append(ModelEvalLog(epoch, batch_number, loss, metric))
 
     def log_test_score(self, epoch, batch_number=None):
+        """ Log the current testing score of the model """
         if self.x_test is None or self.y_test is None:  # Do nothing
             return None
 
@@ -39,6 +72,15 @@ class ModelLogger:
         self.test_history.append(ModelEvalLog(epoch, batch_number, loss, metric))
 
     def save(self, train_filename=None, test_filename=None):
+        """ Save the training and testing history as csv files
+
+            Parameters
+            ----------
+            train_filename : str, optional
+                The file name to save the training history
+            test_filename : str, optional
+                The file name to save the testing history
+        """
         if train_filename is None:
             train_filename = 'train_log.csv'
         if test_filename is None:
@@ -49,6 +91,13 @@ class ModelLogger:
 
     @property
     def train_history_as_df(self):
+        """ Converts the attribute `train_history` to a pd.DataFrame.
+
+            Notes
+            -----
+            The purpose of converting `train_history` to a dataframe is that
+            it allows quicker saving of the training history
+        """
         if len(self.train_history) == 0:
             return ModelEvalLog(None, None, None, None).as_dataframe()
 
@@ -57,6 +106,13 @@ class ModelLogger:
 
     @property
     def test_history_as_df(self):
+        """ Converts the attribute `test_history` to a pd.DataFrame.
+
+            Notes
+            -----
+            The purpose of converting `test_history` to a dataframe is that
+            it allows quicker saving of the testing history
+        """
         if len(self.test_history) == 0:
             return ModelEvalLog(None, None, None, None).as_dataframe()
 
@@ -65,6 +121,12 @@ class ModelLogger:
 
 
 class Log:
+    """ Base class for a logger
+
+        Notes
+        -----
+        Initialising only accepts keyword arguments
+    """
     def __init__(self, **kwargs):
         self.log = kwargs
 
@@ -73,7 +135,34 @@ class Log:
 
 
 class ModelEvalLog(Log):
+    """ Logger for :obj:Model
+
+        Attributes
+        ----------
+        epoch : int
+            The current number of epochs the model has been trained
+        batch_number : int or None
+            The current batch number, within the epoch. If the batch number
+            isn't being counted, then set this to None
+        loss : float
+            The loss on the dataset
+        metric : float
+            The metric on the dataset
+        other : dict of str - :obj:
+            Other keyword arguments, this should be used if there
+            is something else that you want to be saved
+    """
     def __init__(self, epoch, batch_number, loss, metric, **kwargs):
+        """ Initialise the Log
+
+            Notes
+            -----
+            Unlike :obj:Log, ModelEvalLog assumes to have the 4 base attributes
+                epoch, batch_number, loss, metric
+
+            If you want to save other attributes/features, then use keyword
+            arguments to save it.
+        """
         super().__init__(loss=loss, metric=metric, **kwargs)
 
         self.epoch = epoch
