fd3ba207de3d9a2dad5061cd52f1c743613850d8
infamoussoap <james.chok1000+github@gmail.com>
2021-07-23 22:04:46 +1000

    some refactoring to give better error messages

--- PySoap2_gpu/Exceptions.py
+++ PySoap2_gpu/Exceptions.py
@@ -1,7 +1,14 @@
+import pyopencl.array as cl_array
+
+
 class InvalidContextError(Exception):
     pass
 
 
 def check_for_valid_context(context, *args):
-    if any([arg.context != context for arg in args]):
-        raise InvalidContextError("Input has incompatible context")
+    try:
+        if any([arg.context != context for arg in args]):
+            raise InvalidContextError("Input has incompatible context")
+    except AttributeError:
+        invalid_args = [type(arg).__name__ for arg in args if not isinstance(arg, cl_array.Array)]
+        raise ValueError(f"Was expecting instance of cl_array.Array, instead got {set(invalid_args)}")
