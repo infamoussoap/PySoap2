59adeb8d5911ec60e90777081ea624880ef56b6b
infamoussoap <james.chok1000+github@gmail.com>
2021-07-23 18:18:48 +1000

    adding function to check for valid cl_array.Array context

--- PySoap2_gpu/Exceptions.py
+++ PySoap2_gpu/Exceptions.py
@@ -1,2 +1,7 @@
 class InvalidContextError(Exception):
     pass
+
+
+def check_for_valid_context(context, *args):
+    if any([arg.context != context for arg in args]):
+        raise InvalidContextError("Input has incompatible context")
