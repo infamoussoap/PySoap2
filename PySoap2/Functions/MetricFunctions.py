873e6441b857c9eb7ce5a15f2252b08dfe1009dc
infamoussoap <james.chok1000+github@gmail.com>
2021-07-26 10:46:28 +1000

    accuracy-cpu needed to add another condition in the if statement

--- PySoap2/Functions/MetricFunctions.py
+++ PySoap2/Functions/MetricFunctions.py
@@ -2,7 +2,7 @@ import numpy as np
 
 
 def accuracy(predictions, target):
-    if len(target.shape) == 2:
+    if len(target.shape) == 2 and target.shape[1] == 1:
         """ target assumed to be shape (n, 1), so argmax doesnt work """
         predicted_labels = (predictions > 0.5).astype(int)
         return np.mean(predicted_labels == target.astype(int))
