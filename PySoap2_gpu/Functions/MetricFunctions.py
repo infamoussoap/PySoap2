0bee86c699952c17180a1dc9792f5bc9c6876cc4
infamoussoap <james.chok1000+github@gmail.com>
2021-07-26 10:47:03 +1000

    accuracy-gpu now works for (n, 1) targets

--- PySoap2_gpu/Functions/MetricFunctions.py
+++ PySoap2_gpu/Functions/MetricFunctions.py
@@ -1,13 +1,19 @@
-import numpy as np
+import pyopencl.array as cl_array
 
 from PySoap2_gpu.utils import ClArrayTricks
 
 
 def accuracy(predictions, target):
+    N = len(target)
+
+    if len(target.shape) == 2 and target.shape[1] == 1:
+        """ target assumed to be shape (n, 1), so argmax doesnt work """
+        predicted_labels = (predictions > 0.5).astype(int)
+        return cl_array.sum(predicted_labels == target.astype(int)) / N
+
     predicted_labels = ClArrayTricks.arg_max_across_last_axis(predictions)
     target_labels = ClArrayTricks.arg_max_across_last_axis(target)
-
-    return np.mean((predicted_labels == target_labels).get())
+    return cl_array.sum(predicted_labels == target_labels) / N
 
 
 metric_functions = {'accuracy': accuracy}
